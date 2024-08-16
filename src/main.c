d5e6dd6d9f6dc718699cc5d32fc43e73fbebf275
foxblock <foxblock+github@gmail.com>
2024-08-17 00:25:34 +0200

    Add enemies (spawn with space)

--- src/main.c
+++ src/main.c
@@ -34,6 +34,15 @@ typedef struct Home
     int health;
 } Home;
 
+#define ENEMY_SIZE 20
+typedef struct Enemy
+{
+    Vector2 pos; // center
+    Vector2 speed;
+    float health;
+    bool alive;
+} Enemy;
+
 #define FONT_SIZE 20
 
 int main(void)
@@ -53,8 +62,8 @@ int main(void)
 
     SetTargetFPS(60);
 
-    const int MAX_TOWERS = 20;
-    Tower *towers = calloc(MAX_TOWERS, sizeof(Tower));
+    const int MAX_TOWERS = 32;
+    Tower *towers = calloc(MAX_TOWERS, sizeof(towers[0]));
     int towerLen = 0;
     int currentType = ET_SUB;
     int currentScale = 1;
@@ -63,6 +72,10 @@ int main(void)
         .health = 10,
     };
     Rectangle path = {100, 200, screenWidth - 100, TOWER_SIZE};
+
+    const int MAX_ENEMIES = 1024;
+    Enemy *enemies = calloc(MAX_ENEMIES, sizeof(enemies[0]));
+    int enemiesLen = 0;
 
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
@@ -88,6 +101,16 @@ int main(void)
         else if (IsKeyPressed(KEY_DOWN) && currentScale > 0)
             --currentScale;
 
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            enemies[enemiesLen++] = (Enemy){
+                .pos = {screenWidth + 50, screenHeight / 2},
+                .speed = {-0.5, 0},
+                .health = 10,
+                .alive = true,
+            };
+        }
+
         bool posValid = !CheckCollisionPointRec(GetMousePosition(), path);
         posValid &= !CheckCollisionPointRec(GetMousePosition(), home.rect);
         for (int i = 0; i < towerLen; ++i) {
@@ -103,6 +126,24 @@ int main(void)
             };
         }
 
+        // Logic
+        for (int i = 0; i < enemiesLen; ++i)
+        {
+            Enemy *e = enemies + i;
+            if (!e->alive)
+                continue;
+
+            if (CheckCollisionPointRec(e->pos, home.rect))
+            {
+                --home.health;
+                e->alive = false;
+                continue;
+            }
+
+            e->pos.x += e->speed.x;
+            e->pos.y += e->speed.y;
+        }
+
         // Draw
         BeginDrawing();
 
@@ -115,24 +156,24 @@ int main(void)
         // placement preview
         DrawRectangle(tileX * TOWER_SIZE, tileY * TOWER_SIZE, TOWER_SIZE, TOWER_SIZE, posValid ? GRAY : MAROON);
 
+        // Towers
         char text[64] = "";
         int textWidthPixels = 0;
         for (int i = 0; i < towerLen; ++i) {
             assert(currentType < ET_EOL);
 
             Tower t = towers[i];
-
             DrawRectangleRec(t.rect, DARKGRAY);
-
             snprintf(text, sizeof(text), SIGNS[t.type], t.scale);
             textWidthPixels = MeasureText(text, 20);
             DrawText(text, 
                 t.rect.x + (TOWER_SIZE - textWidthPixels) / 2,
                 t.rect.y + (TOWER_SIZE - FONT_SIZE) / 2,
                 FONT_SIZE,
-                BLACK);
+                WHITE);
         }
 
+        // Home
         DrawRectangleRec(home.rect, RED);
         snprintf(text, sizeof(text), "%d", home.health);
         textWidthPixels = MeasureText(text, 20);
@@ -142,10 +183,33 @@ int main(void)
             FONT_SIZE,
             BLACK);
 
+        // Enemies
+        int aliveCount = 0;
+        for (int i = 0; i < enemiesLen; ++i)
+        {
+            Enemy e = enemies[i];
+            if (!e.alive)
+                continue;
+
+            ++aliveCount;
+            DrawCircleV(e.pos, ENEMY_SIZE, SKYBLUE);
+            snprintf(text, sizeof(text), "%.2g", e.health);
+            textWidthPixels = MeasureText(text, 20);
+            DrawText(text, 
+                e.pos.x - textWidthPixels / 2,
+                e.pos.y - FONT_SIZE / 2,
+                FONT_SIZE,
+                BLACK);
+        }
+
         EndMode2D();
 
+        // GUI
         int yPos = 4;
         snprintf(text, sizeof(text), "Towers: %d / %d", towerLen, MAX_TOWERS);
+        DrawText(text, 4, yPos, 20, BLACK);
+        yPos += 24;
+        snprintf(text, sizeof(text), "Enemies: %d", aliveCount);
         DrawText(text, 4, yPos, 20, BLACK);
         yPos += 24;
         snprintf(text, sizeof(text), "Current sign: %s", SIGNS[currentType]);
@@ -153,6 +217,8 @@ int main(void)
         yPos += 24;
         snprintf(text, sizeof(text), "Current scale: %d", currentScale);
         DrawText(text, 4, yPos, 20, BLACK);
+
+        DrawFPS(screenWidth - 80, 0);
 
         EndDrawing();
     }
